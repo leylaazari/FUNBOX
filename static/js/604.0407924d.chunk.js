27cd12b8034a4004bc17812090d5d9f71750195c
Leyla Azari <leyla.azari@gmail.com>
2022-10-31 13:49:14 +0330

    Updates

---
+++ static/js/604.0407924d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfe_hiring=self.webpackChunkfe_hiring||[]).push([[604],{6223:function(e,n,t){t.r(n);var i=t(1253),u=t(184);n.default=function(){return(0,u.jsx)(i.Z,{title:"watch"})}}}]);
+//# sourceMappingURL=604.0407924d.chunk.js.map
---
+++ static/js/768.b4a0bd73.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfe_hiring=self.webpackChunkfe_hiring||[]).push([[768],{9166:function(e,i,n){n.r(i);var t=n(1253),u=n(184);i.default=function(){return(0,u.jsx)(t.Z,{title:"favourite"})}}}]);
+//# sourceMappingURL=768.b4a0bd73.chunk.js.map
